962d17f6b9672893ba8ab7b89e236f3cd1f5fd1a
hoxnox <hoxnox@gmail.com>
2017-05-03 13:35:03 +0300

    ENH: nxtools update

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,12 @@
-from nxtools import NxConanFile, retrieve
-from conans import CMake
+from nxtools import NxConanFile
+from conans import CMake, tools
 
 
 class DocoptCppConan(NxConanFile):
     name = "docopt.cpp"
     version = "0.6.2"
     license = "MIT"
-    url = "https://github.com/docopt/docopt.cpp"
+    url = "https://github.com/hoxnox/conan-docopt.cpp"
     license = "https://github.com/docopt/docopt.cpp/blob/master/LICENSE-MIT"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared":[True, False]}
@@ -15,7 +15,7 @@ class DocoptCppConan(NxConanFile):
     description = "docopt creates beautiful command-line interfaces."
 
     def do_source(self):
-        retrieve("c05542245232420d735c7699098b1ea130e3a92bade473b64baf876cdf098a17",
+        self.retrieve("c05542245232420d735c7699098b1ea130e3a92bade473b64baf876cdf098a17",
                 [
                     "vendor://docopt/docopt.cpp/docopt.cpp-{v}.tar.gz".format(v=self.version),
                     "https://github.com/docopt/docopt.cpp/archive/v{v}.tar.gz".format(v=self.version)
@@ -24,6 +24,8 @@ class DocoptCppConan(NxConanFile):
 
     def do_build(self):
         cmake = CMake(self)
+        cmake.build_dir = "{staging_dir}/src".format(staging_dir=self.staging_dir)
+        tools.untargz("docopt.cpp-{v}.tar.gz".format(v=self.version), cmake.build_dir)
         cmake.configure(defs={
                 "CMAKE_INSTALL_PREFIX": self.package_folder,
                 "CMAKE_INSTALL_LIBDIR": "lib"
